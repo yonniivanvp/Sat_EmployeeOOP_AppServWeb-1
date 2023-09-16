b3f3432eb47e574d335129542716f8eff7419096
Yonnier Ivan Vega Palacios <yonnivanvp@hotmail.com>
2023-09-16 18:55:29 -0500

    Create Employee class

---
+++ EmployeeOOP/Classes/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Classes
+{
+    public abstract class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; } //Signo de interrogacion (?) significa que puede tener dato o no
+
+        public Date? BirthDate { get; set; }
+
+        public Date? HiringDate { get; set; }
+
+        public bool? IsActive { get; set; }
+        #endregion
+
+        #region Methods
+
+        public abstract decimal GetValueToPay();
+
+        #endregion
+    }
+}
